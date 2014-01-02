70bd1236fa5a61db449bc5a49c075801961243fd
Anttonen, Ilkka <ilkka.anttonen@accenture.com>
2014-01-02 02:34:17 +0200

    Created the startup script files for nodens and riak which set up a local networking with static IP addresses. Added the Dockerfile for creating the nodens instance.

---
+++ virtualization/vagrant_ansible_docker/dockerfiles/riak/startRiak.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Starting the riak container
+
+RIAK_CONTAINER=$(sudo docker run -d -p 8098:8098 -p 8087:8087 -p 1002:22 -dns=10.1.1.1 -dns=8.8.8.8 test/riak)
+sudo pipework br1 $RIAK_CONTAINER 10.1.1.2/24
+
+# Register the host to the dns server
+curl -X PUT -H "Content-Type: application/json" http://localhost:8053/lookup -d '{ "address": "10.1.1.2", "name": "1.riak.local", "ttl": "10", "expires": "3600", "type": 1 }'
