ec74450d0bf98fb6d4a129ddd2377e3321e387ce
Anttonen, Ilkka <ilkka.anttonen@accenture.com>
2014-01-18 04:30:50 +0200

    Finally got the Cassandra cluster to work.

--- virtualization/vagrant_ansible_docker/dockerfiles/nodens/startNodens.sh
+++ virtualization/vagrant_ansible_docker/dockerfiles/nodens/startNodens.sh
@@ -2,4 +2,4 @@
 echo Starting the nodens container
 
 NODENS_CONTAINER=$(sudo docker run -d -p 8053:8053 -p 1001:22 -e DNSPORT=53 test/nodens)
-sudo pipework br1 $NODENS_CONTAINER 10.1.1.1/24
+sudo pipework br1 $NODENS_CONTAINER 10.1.1.1/22
