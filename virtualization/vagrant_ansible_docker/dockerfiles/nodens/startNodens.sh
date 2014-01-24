046c3d6cf89b6d1672c6664d8e1f69b4ec21dfda
Oscar Renalias <oscar@renalias.net>
2014-01-25 00:27:07 +0200

    Build the container automatically if the script detects that it has not been built yet

--- virtualization/vagrant_ansible_docker/dockerfiles/nodens/startNodens.sh
+++ virtualization/vagrant_ansible_docker/dockerfiles/nodens/startNodens.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-echo Starting the nodens container
+IMAGE_NAME=test/nodens
+echo "== Checking if the container for NodeNS has already been built."
+docker images | grep "^$IMAGE_NAME"
+if [ $? == 1 ]; then
+	echo "== Container has not been built, building now."
+	docker build -t $IMAGE_NAME . 
+else
+	echo "== Container has been built; skipping build step."
+fi
+
+echo "== Starting the nodens container."
 
 NODENS_CONTAINER=$(sudo docker run -d -p 8053:8053 -p 1001:22 -e DNSPORT=53 test/nodens)
 sudo pipework br1 $NODENS_CONTAINER 10.1.1.1/22
