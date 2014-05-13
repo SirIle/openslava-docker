625e3dd16bb6facc8dacd3d3debf09d5864ee555
unknown <ilkka.anttonen@MW7H48G7BH3L1P.dir.svc.accenture.com>
2014-05-13 16:52:14 +0300

    Added the consul ui and configured ports so that ui and consul interface are accessible from outside of the containers.

--- virtualization/vagrant_docker/dockerfiles/core/startCore.sh
+++ virtualization/vagrant_docker/dockerfiles/core/startCore.sh
@@ -11,5 +11,5 @@ fi
 
 echo "== Starting the core container."
 
-NODENS_CONTAINER=$(sudo docker run -d -p 1000:22 -h core --dns 127.0.0.1 $IMAGE_NAME)
-sudo pipework br1 $NODENS_CONTAINER 10.1.1.1/21
+CORE_CONTAINER=$(sudo docker run -d -p 1000:22 -p 9200:9200 -p 80:80 -p 8500:8500 -h core --dns 127.0.0.1 $IMAGE_NAME)
+sudo pipework br1 $CORE_CONTAINER 10.1.1.1/21
