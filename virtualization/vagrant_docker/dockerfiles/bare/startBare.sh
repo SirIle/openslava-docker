ea43a66c5dbe8fe1d5a09f80fc7d55bdb897cb6b
Anttonen, Ilkka <ilkka.anttonen@accenture.com>
2014-05-14 21:15:37 +0300

    Removed pipework as it's no longer needed. Added named container linking for Consul discovery. Updated README-files.

--- virtualization/vagrant_docker/dockerfiles/bare/startBare.sh
+++ virtualization/vagrant_docker/dockerfiles/bare/startBare.sh
@@ -17,19 +17,17 @@ check_usage $# 1 "Usage: $0 <NODE_NUMBER>"
 NODE=$1
 IMAGE_NAME=test/bare
 HOSTNAME="bare$NODE"
-IP=10.1.3.$NODE
 PORTS="-p 103$NODE:22"
 
 echo "== Checking if the bare container has already been built."
 sudo docker images | grep "^$IMAGE_NAME"
 if [ $? == 1 ]; then
 	echo "== Container has not been built, building now."
-	sudo docker build -t $IMAGE_NAME . 
+	sudo docker build -t $IMAGE_NAME .
 else
 	echo "== Container has been built; skipping build step."
 fi
 
-echo "== Starting bare instance $NODE, hostname $HOSTNAME, ip $IP, ssh port 103$NODE"
+echo "== Starting bare instance $NODE, hostname $HOSTNAME, ssh port 103$NODE"
 
-cid=$(sudo docker run -d $PORTS -h $HOSTNAME --dns 127.0.0.1 $IMAGE_NAME)
-sudo pipework br1 $cid $IP/21
+sudo docker run -d $PORTS -h $HOSTNAME --dns 127.0.0.1 --link core:core $IMAGE_NAME
