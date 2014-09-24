f42136705cc17f7681f1fbd516371b29028d4b40
unknown <ilkka.anttonen@MW7S9AB6B8CK9P.dir.svc.accenture.com>
2014-09-24 09:23:06 +0300

    Upgraded Cassandra to 2.1.0.

--- dockerfiles/cassandra/files/startCassandra.sh
+++ dockerfiles/cassandra/files/startCassandra.sh
@@ -4,4 +4,6 @@ then
   IP=`hostname -i`
 fi
 sed -i "s/^listen_address:.*/listen_address: $IP/" /etc/cassandra/cassandra.yaml
+sed -i "s/^rpc_address:.*/rpc_address: 0.0.0.0/" /etc/cassandra/cassandra.yaml
+sed -i "s/^# broadcast_rpc_address:.*/broadcast_rpc_address: $IP/" /etc/cassandra/cassandra.yaml
 exec /usr/sbin/cassandra -f start
